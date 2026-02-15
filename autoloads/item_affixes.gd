d0745dcedae2c72db34253a0547421069fa13ae6
V. Kurta <vincent.kurta@hotmail.fr>
2026-02-15 03:12:16 +0100

    feat(03-01): add StatType enum and stat_types property to Affix
    - Add StatType enum to tag.gd with 10 stat type entries
    - Add stat_types property to Affix class with default []
    - Update Affix._init() to accept stat_types parameter as last argument
    - Populate all 24 affix definitions in item_affixes.gd with stat_types
    - Update Affixes.from_affix() to copy stat_types to new instances
    - Existing AffixTag string constants remain unchanged for filtering

--- autoloads/item_affixes.gd
+++ autoloads/item_affixes.gd
@@ -2,50 +2,89 @@ class_name Affixes extends Node
 
 var prefixes: Array[Affix] = [
 	Affix.new(
-		"Physical Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.PHYSICAL, Tag.FLAT, Tag.WEAPON]
+		"Physical Damage",
+		Affix.AffixType.PREFIX,
+		2,
+		10,
+		[Tag.PHYSICAL, Tag.FLAT, Tag.WEAPON],
+		[Tag.StatType.FLAT_DAMAGE]
 	),
 	Affix.new(
 		"%Physical Damage",
 		Affix.AffixType.PREFIX,
 		2,
 		10,
-		[Tag.PHYSICAL, Tag.PERCENTAGE, Tag.WEAPON]
+		[Tag.PHYSICAL, Tag.PERCENTAGE, Tag.WEAPON],
+		[Tag.StatType.INCREASED_DAMAGE]
 	),
-	Affix.new("%Elemental Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON]),
-	Affix.new("%Cold Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON]),
-	Affix.new("%Fire Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON]),
-	Affix.new("%Lightning Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON]),
+	Affix.new("%Elemental Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON], []),
+	Affix.new("%Cold Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON], []),
+	Affix.new("%Fire Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON], []),
+	Affix.new("%Lightning Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.WEAPON], []),
 	Affix.new(
 		"Lightning Damage",
 		Affix.AffixType.PREFIX,
 		2,
 		10,
-		[Tag.ELEMENTAL, Tag.LIGHTNING, Tag.WEAPON]
+		[Tag.ELEMENTAL, Tag.LIGHTNING, Tag.WEAPON],
+		[]
 	),
-	Affix.new("Fire Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.FIRE, Tag.WEAPON]),
-	Affix.new("Cold Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.COLD, Tag.WEAPON]),
+	Affix.new(
+		"Fire Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.FIRE, Tag.WEAPON], []
+	),
+	Affix.new(
+		"Cold Damage", Affix.AffixType.PREFIX, 2, 10, [Tag.ELEMENTAL, Tag.COLD, Tag.WEAPON], []
+	),
 ]
 var suffixes: Array[Affix] = [
-	Affix.new("Attack Speed", Affix.AffixType.SUFFIX, 2, 10, [Tag.SPEED, Tag.ATTACK, Tag.WEAPON]),
-	Affix.new("Cast Speed", Affix.AffixType.SUFFIX, 2, 10, [Tag.MAGIC, Tag.WEAPON]),
-	Affix.new("Damage over time", Affix.AffixType.SUFFIX, 2, 10, [Tag.DOT, Tag.WEAPON]),
-	Affix.new("Bleed Damage", Affix.AffixType.SUFFIX, 2, 10, [Tag.DOT, Tag.PHYSICAL, Tag.WEAPON]),
-	Affix.new("Life", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Sigil", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE, Tag.MAGIC]),
-	Affix.new("Evade", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Armor", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Physical Reduction", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Magical Reduction", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Elemental Reduction", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Dodge Chance", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Dmg Suppression Chance", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE]),
-	Affix.new("Critical Strike Chance", Affix.AffixType.SUFFIX, 2, 10, [Tag.CRITICAL]),
-	Affix.new("Critical Strike Damage", Affix.AffixType.SUFFIX, 2, 10, [Tag.CRITICAL]),
+	Affix.new(
+		"Attack Speed",
+		Affix.AffixType.SUFFIX,
+		2,
+		10,
+		[Tag.SPEED, Tag.ATTACK, Tag.WEAPON],
+		[Tag.StatType.INCREASED_SPEED]
+	),
+	Affix.new("Cast Speed", Affix.AffixType.SUFFIX, 2, 10, [Tag.MAGIC, Tag.WEAPON], []),
+	Affix.new("Damage over time", Affix.AffixType.SUFFIX, 2, 10, [Tag.DOT, Tag.WEAPON], []),
+	Affix.new(
+		"Bleed Damage", Affix.AffixType.SUFFIX, 2, 10, [Tag.DOT, Tag.PHYSICAL, Tag.WEAPON], []
+	),
+	Affix.new("Life", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], [Tag.StatType.FLAT_HEALTH]),
+	Affix.new("Sigil", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE, Tag.MAGIC], []),
+	Affix.new("Evade", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], []),
+	Affix.new("Armor", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], [Tag.StatType.FLAT_ARMOR]),
+	Affix.new("Physical Reduction", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], []),
+	Affix.new("Magical Reduction", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], []),
+	Affix.new("Elemental Reduction", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], []),
+	Affix.new("Dodge Chance", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], []),
+	Affix.new("Dmg Suppression Chance", Affix.AffixType.SUFFIX, 2, 10, [Tag.DEFENSE], []),
+	Affix.new(
+		"Critical Strike Chance",
+		Affix.AffixType.SUFFIX,
+		2,
+		10,
+		[Tag.CRITICAL],
+		[Tag.StatType.CRIT_CHANCE]
+	),
+	Affix.new(
+		"Critical Strike Damage",
+		Affix.AffixType.SUFFIX,
+		2,
+		10,
+		[Tag.CRITICAL],
+		[Tag.StatType.CRIT_DAMAGE]
+	),
 ]
 
 
 static func from_affix(template: Affix) -> Affix:
 	var affix_copy = Affix.new(
-		template.affix_name, template.type, template.min_value, template.max_value, template.tags
+		template.affix_name,
+		template.type,
+		template.min_value,
+		template.max_value,
+		template.tags,
+		template.stat_types
 	)
 	return affix_copy
--- models/affixes/affix.gd
+++ models/affixes/affix.gd
@@ -9,6 +9,7 @@ var max_value: int
 var value: int
 var tier: int
 var tags: Array[String]
+var stat_types: Array[int] = []
 
 
 func _init(
@@ -16,12 +17,14 @@ func _init(
 	p_type: AffixType = AffixType.PREFIX,
 	p_min: int = 0,
 	p_max: int = 0,
-	p_tags: Array[String] = []
+	p_tags: Array[String] = [],
+	p_stat_types: Array[int] = []
 ) -> void:
 	self.affix_name = p_name
 	self.type = p_type
 	self.tier = randi_range(1, 8)
 	self.tags = p_tags
+	self.stat_types = p_stat_types
 	# Tier 1 is highest, so higher tier numbers = lower values
 	self.min_value = p_min * (9 - tier)
 	self.max_value = p_max * (9 - tier)
